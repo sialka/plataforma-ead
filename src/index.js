d2f84c84e6699b4016e9426c9a7572722d79658d
Sidnei <sialkas@gmail.com>
2019-08-06 17:47:48 -0300

    Usando dotenv-safe

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+require('dotenv-safe').config();
+
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
   });
 
   server.route({
